885de91011e455713f116d536bf495e89dbb2fc8
Sasha Maksyutenko <sasha.maksyutenko@gmail.com>
2024-06-28 01:53:01 +0300

    contactopedia (CRUD Operations in React)

---
+++ contactopedia/src/Components/ContactPages/RemoveAllContacts.jsx
@@ -0,0 +1,8 @@
+const RemoveAllContacts=(props)=>{
+    return(
+        <div>
+            <button className="btn btn-danger form-control"onClick={()=>props.handleRemoveAllContacts()}>Remove All</button>
+        </div>
+    )
+}
+export default RemoveAllContacts
--- contactopedia/src/index.js
+++ contactopedia/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import ContactIndex from '../../contactopedia/src/Components/ContactPages/ContactIndex';
+import ContactIndex from './Components/ContactPages/ContactIndex';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  
     <ContactIndex/>
+ 
 );
 
 
